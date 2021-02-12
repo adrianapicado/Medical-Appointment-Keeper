d444cf6adc44ee0256aa8952290fa5a26d81a846
adriana <picadoadriana7@gmail.com>
2021-02-12 17:45:52 -0500

    add user  post route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,21 +1,30 @@
 class UserController < ApplicationController
 
     get '/user/signup' do 
-           erb :'/user/new'        
-      end 
+        erb :'/user/new'        
+    end
+    
+    post '/user/signup' do 
+    @user = User.create(
+        name: params[:name],
+        username: params[:username], 
+        password: params[:password])
+    redirect "user/#{@user.id}"
+    end
 
     get '/user/login' do 
         erb :'/user/login'  
     end
+
+    get '/user/:id' do 
+       @user = User.find(params[:id])
+       erb :'/user/show'
+     end 
+
     
-
-
-        
-         
-  
-      
-  
-       
-  
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
   
   end 
