64d2f4d966b65e022fbef0c7e3549e4d5faf40c3
adriana <picadoadriana7@gmail.com>
2021-02-11 22:18:18 -0500

    update app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,28 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+configure do
+   set :public_folder, 'public'
+   set :views, 'app/views'
+   enable :sessions
+   set :session_secret, ENV['SESSION_SECRET']
+ end
 
-  get "/" do
-    erb :welcome
-  end
+helpers do 
+  
+  def logged_in?      
+    !!session[:user_id]
+  end 
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end 
+
+end 
+
+get "/" do
+  erb :welcome
+end
+
 
 end
