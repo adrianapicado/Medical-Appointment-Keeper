4d52c96114c83d42fc82d01762c06f51739eb4d6
adriana <picadoadriana7@gmail.com>
2021-02-12 17:59:08 -0500

    map crud

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -1,2 +1,34 @@
 class AppointmentController < ApplicationController
+    ##create##
+
+    get 'appointment/new' do #render form 
+
+    end
+
+    post '/appointments' do #process form 
+        
+    end
+
+
+
+
+
+
+    ##read##
+
+
+
+
+
+    ##update##
+
+
+
+
+
+    ##delete##
+
+
+
+
 end
