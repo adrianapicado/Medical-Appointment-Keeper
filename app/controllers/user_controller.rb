cd2163b20350a448c89aeaddedfb9a7cef1a97c7
adriana <picadoadriana7@gmail.com>
2021-02-22 18:11:13 -0500

    update user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,21 +5,28 @@ class UserController < ApplicationController
     end
     
     post '/user/signup' do 
-    @user = User.create(
-        name: params[:name],
-        username: params[:username], 
-        password: params[:password])
-    redirect "user/#{@user.id}"
+        if !params[:username].empty? && !params[:password].empty? && !User.find_by_username(params[:username])
+           User.create(username: params[:username], password: params[:password])
+           session[:user_id] = user.id
+           redirect "/appointments"
+        else
+            redirect "/user/signup"
+        end
     end
 
     get '/user/login' do 
         erb :'/user/login'  
     end
 
-    get '/user/:id' do 
-       @user = User.find(params[:id])
-       erb :'/user/show'
-     end 
+    post '/user/login' do
+    end
+
+
+
+    # get '/user/:id' do 
+    #    @user = User.find(params[:id])
+    #    erb :'/user/show'
+    #  end 
 
     
     get '/logout' do
